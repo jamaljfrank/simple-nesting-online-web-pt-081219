a7ac6c892e10a69e28404325e61aa604ad0a68ad
Jamal Frank <jamaljfrank@gmail.com>
2019-09-10 20:33:08 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -128,7 +128,9 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages] = ["C", "Assembly"]
+
+languages = programmer_hash[:dennis_ritchie][:languages]
+languages << "Assembly"
 programmer_hash
 end
 
