8d1817b49d456271814f818da3532e895d319aba
Jamal Frank <jamaljfrank@gmail.com>
2019-09-10 20:19:56 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -58,7 +58,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-     language = programmer_hash[:dennis_ritchie][:languages] 
+     language = programmer_hash[:dennis_ritchie][:languages][0] 
      language[0]
 
 end
